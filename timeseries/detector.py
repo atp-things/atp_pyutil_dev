f4252288520ce145a8013822ee782552e2698e3a
andrazpolak <andrazpolak@gmail.com>
2022-07-12 14:51:49 +0200

    done

---
+++ timeseries/detector.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+
+def rising_edge_shift(df: pd.Series, trigger, output: type = int) -> pd.Series:
+
+    ret = (df <= trigger) & (df.shift(-1) >= trigger)
+
+    if output == int:
+        ret = ret.replace({True: 1, False: 0})
+
+    return ret
+
+
+def falling_edge_shift(df: pd.Series, trigger, output: type = int) -> pd.Series:
+    ret = (df >= trigger) & (df.shift(-1) <= trigger)
+    if output == int:
+        ret = ret.replace({True: 1, False: 0})
+    return ret
+
+
+def rising_and_falling_edge_shift(
+    df: pd.Series, trigger, output: type = int
+) -> pd.Series:
+    ret = pd.DataFrame(
+        {
+            "rising_edge": rising_edge_shift(df, trigger),
+            "falling_edge": falling_edge_shift(df, trigger),
+        }
+    )
+    ret = ret.any(axis="columns")
+
+    if output == int:
+        ret = ret.replace({True: 1, False: 0})
+
+    return ret
---
+++ timeseries/filter.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+
+def cut_below(df: pd.Series, threshold, to=0) -> pd.Series:
+    ret = df
+    ret.loc[ret < threshold] = to
+    return ret
+
+
+def cut_above(df: pd.Series, threshold, to=0) -> pd.Series:
+    ret = df
+    ret.loc[ret > threshold] = to
+    return ret
