0781580ebac0e3d3de0f84780a112066451aeac6
andrazpolak <andrazpolak@gmail.com>
2022-09-26 17:57:21 +0200

    fd

--- dict_tools.py
+++ dict_tools.py
@@ -6,19 +6,20 @@ def get_keys(dict_: dict, keys: list) -> dict:
     return {key: dict_[key] for key in dict_.keys() & keys}
 
 
-def get_key_list(dict_: dict) -> list:
+def get_key_as_list(dict_: dict) -> list:
+    """In ATP Things"""
     return [key for key in dict_.keys()]
 
 
 def load(path: str) -> dict:
+    """In ATP Things"""
     with open(path) as file_:
         ret = dict(json.load(file_))
-
     return ret
 
 
 def get_values_from_list(list_of_dict: list, key: str, unique: bool = False):
-
+    """In ATP Things"""
     list_ = []
     for item in list_of_dict:
         list_.append(item[key])
@@ -43,7 +44,7 @@ def convert_list_to_dict(list_of_dict: list, key: str):
     return dict_
 
 
-def dictionary_from_list(list_of_dict: list, key: str, value: str):
+def dictionary_from_list(list_of_dict: list, key: str, value: str = None):
 
     dict_ = {}
     duplicates_counter = 0
